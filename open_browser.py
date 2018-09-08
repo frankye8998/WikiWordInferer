58f9961b4996347a983e6aef3d4539aa0e030291
frankye8998 <frankfrankfrankyeyeye@gmail.com>
2018-09-08 15:31:36 -0400

    Added CSS

--- open_browser.py
+++ open_browser.py
@@ -1,7 +1,7 @@
 from find_article import findarticle
 import webbrowser
 
-content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".replace("\n","<br>")
+content = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>".replace("\n","<br>")
 
 title = "<title>Lorem Ipsum</title>"
 
@@ -9,4 +9,4 @@ favicon = '<link rel="shortcut icon" type="image/png" href="https://en.wikipedia
 
 article_title = "<h1>Lorem Ipsum Ur mom Big Dumb</h1><br>"
 
-webbrowser.open("data:text/html,<html><head>" + title + favicon + "</head><body><p>" + article_title + content + "</p></body></html>")
+webbrowser.open('data:text/html,<html><head><style>body{margin: 5%;}h1{font-family: "Times New Roman", Times, Seriff; font-weight: lighter;}</style>' + title + favicon + "</head><body>" + article_title + content + "</body></html>")
