bd84fb77b2d2aef594aaf0d331ef93d97b02bebd
frankye8998 <frankfrankfrankyeyeye@gmail.com>
2018-09-08 14:11:56 -0400

    Changed how you input the Wikipedia article and gave it a return

--- Find_article.py
+++ Find_article.py
@@ -1,8 +1,7 @@
 import wikipedia
 
 
-def findarticle():
-	article = input("Please enter a article name: ") # This line can be changed to take input from the webpage (or program?)
+def findarticle(article):
 	
 	#Finds the name of the closest article to user input
 	search = wikipedia.search(article)
@@ -11,4 +10,5 @@ def findarticle():
 	#Stores the article as a string (for parsing and finding key words later)
 	x = wikipedia.page(article)
 	str_article = str(x.content)
+	return str_article
 	
