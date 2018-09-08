0ba15fadf83c85863ab6830de4f102c253aaf3b8
frankye8998 <frankfrankfrankyeyeye@gmail.com>
2018-09-08 14:46:16 -0400

    Relies on find_article.py now

--- linktobrowser.py
+++ linktobrowser.py
@@ -1,16 +1,4 @@
-import requests
-import bs4
+from find_article import findarticle
 import webbrowser
-import commonwords
 
-a = requests.get(input()).text
-
-soup = bs4.BeautifulSoup(a,'html.parser')
-
-[s.extract() for s in soup('script')]
-for i in soup("link"):
-  i['href'] = 'http://wikipedia.org' + i['href']
-for i in soup("img"):
-  i['src'] = 'http:' + i['src']
-
-webbrowser.open("data:text/html," + str(soup))
+webbrowser.open("data:text/html,<html><body><p>" + findarticle(input("Article: ")).replace("\n","<br>") + "</p></body></html>")
